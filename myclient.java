264d33d406a1a70854b16979ea79c1b01d7c9f98
pritam285 <pritamjosephjojo@gmail.com>
2016-06-06 16:04:19 +0530

    Add files via upload

---
+++ myclient.java
@@ -0,0 +1,9 @@
+import java.net.*;
+ class myclient
+{
+ public static void main(String arg[]) throws Exception{
+ Socket s=new Socket("192.168.42.253",2020);
+ System.out.println("connection is establised");
+ 
+ }
+}
