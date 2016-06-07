e90b51f4af656d814bde051ffa25c58f471ab892
pritam285 <pritamjosephjojo@gmail.com>
2016-06-07 10:35:04 +0530

    Add files via upload

--- myserver.java
+++ myserver.java
@@ -1,10 +1,13 @@
 import java.net.*;
- class myserver
+
+ class myserverclass
 {
  public static void main(String arg[]) throws Exception{
  ServerSocket s=new ServerSocket(2020);
+
  System.out.println("server waiting for connevcrion");
  Socket so=s.accept();
+
  System.out.println("connection is establised");
  
  }
